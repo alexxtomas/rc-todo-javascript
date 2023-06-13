536907c1ed9ffc4ace29e1c5c3fd368c98326d22
alexxtomas <atomasllimos@gmail.com>
2023-06-13 18:37:21 +0200

    feat: Add new elements and funcionalities to Tasks compnent

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -3,14 +3,15 @@ import { PRIORITIES } from '@utils/constants'
 import './style.css'
 import TaskElement from '@components/TaskElement'
 
-const Tasks = ({ status, tasks, idx }) => {
+const Tasks = ({ color, id, label, tasks }) => {
+  const tasksLength = tasks.length
   return `
-  <div class="tasks-element" id=${status.name}>
+  <div class="tasks-element" id=${id}>
   <header class="tasks-header">
     <div class="tasks-header-container">
-     ${Icon({ variant: ICON_VARIANTS_ENUM.CHEVRON_RIGHT, props: `width=12px height=12px stroke-width="0.3" fill="none" data-function="tasks-accordion-toggle"  class="tasks-accordion-toggle rotate" data-id=${status.name}` })}
-    <h3 data-function="show-tasks-status" class="tasks-status" style="background:${status.color}">${status.name}</h3>
-      <p data-function="show-tasks-counter" class="tasks-counter">${tasks.length} ${tasks.length === 1 ? 'Task' : 'Tasks'}</p>
+     ${Icon({ variant: ICON_VARIANTS_ENUM.CHEVRON_RIGHT, props: `width=12px height=12px stroke-width="0.3" fill="none" data-function="tasks-accordion-toggle"  class="tasks-accordion-toggle rotate" data-id=${id}` })}
+    <h3 data-function="show-tasks-status" class="tasks-status" style="background:${color}">${label}</h3>
+      <p data-function="show-tasks-counter" data-counter=${tasksLength} class="tasks-counter">${tasksLength} ${tasksLength === 1 ? 'Task' : 'Tasks'}</p>
    </div>
     <div data-function="tasks-labels-container" class="tasks-labels-container">
       <h4 class="tasks-label">Creation Date</h4>
@@ -18,10 +19,9 @@ const Tasks = ({ status, tasks, idx }) => {
     </div>
     </header>
   <ul data-function="show-tasks" >
-    ${tasks.map(({ creationDate, name, priority }) => {
-    const iconColor = PRIORITIES[priority.toUpperCase().replaceAll(' ', '_')].COLOR
-
-      return TaskElement({ creationDate, iconColor, name })
+    ${tasks.map(({ id: taskId, creationDate, name, priority }) => {
+      const iconColor = PRIORITIES[priority].color
+      return TaskElement({ id: taskId, creationDate, iconColor, name, statusColor: color })
     }).join('').replaceAll(',', '')}
   
 
