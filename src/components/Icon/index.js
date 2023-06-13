a3ada62cadab1943b199cbd773c960a81632daf3
alexxtomas <atomasllimos@gmail.com>
2023-06-13 18:35:09 +0200

    feat: Add new Icon

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -3,12 +3,14 @@ import ChevronRightIcon from './components/ChevronRightIcon'
 import EditIcon from './components/EditIcon'
 import FlagIcon from './components/FlagIcon'
 import TrashIcon from './components/TrashIcon'
+import XCirlceIcon from './components/XCircleIcon'
 const ICON_VARIANTS = {
   FLAG: FlagIcon,
   TRASH: TrashIcon,
   EDIT: EditIcon,
   ARROW_DOWN_CIRCLE: ArrowDownCircleIcon,
-  CHEVRON_RIGHT: ChevronRightIcon
+  CHEVRON_RIGHT: ChevronRightIcon,
+  XCIRCLE: XCirlceIcon
 }
 
 export const ICON_VARIANTS_ENUM = {
@@ -16,7 +18,8 @@ export const ICON_VARIANTS_ENUM = {
   TRASH: 'TRASH',
   EDIT: 'EDIT',
   ARROW_DOWN_CIRCLE: 'ARROW_DOWN_CIRCLE',
-  CHEVRON_RIGHT: 'CHEVRON_RIGHT'
+  CHEVRON_RIGHT: 'CHEVRON_RIGHT',
+  XCIRCLE: 'XCIRCLE'
 }
 
 const Icon = ({ variant, props }) => {
