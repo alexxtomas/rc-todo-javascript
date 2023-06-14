6b80bab953cb9352dc9d037d7994fef0d23ca2f3
alexxtomas <atomasllimos@gmail.com>
2023-06-14 16:19:02 +0200

    feat: Create function to get button props

--- src/components/TaskElement/utils/functions.js
+++ src/components/TaskElement/utils/functions.js
@@ -1,15 +1,35 @@
+import { BUTTON_VARIANTS_ENUM } from '@components/Button'
 import { DIALOG_VARIANTS_ENUM } from '@components/Dialog'
+import TaskElementDropdownListItems from '../components/TaskElementDropdownContent'
+import Icon, { ICON_VARIANTS_ENUM } from '@components/Icon'
 
-export const getConfirmDialogProps = ({ taskName }) => ({
-  variant: DIALOG_VARIANTS_ENUM.CONFIRMATION,
-  dialogAttributes: 'data-function="remove-task-element-dialog"',
-  text: `Are you sure you want to delete the "${taskName}" task ?`,
-  firstButton: {
-    attributes: 'data-function="remove-task-element-button"',
-    text: 'Yes'
+export const getDropdownButtonProps = ({ iconColor, id }) => ({
+  variant: BUTTON_VARIANTS_ENUM.DROP_DOWN,
+
+  tooltip: {
+    text: 'Set priority',
+    attributes: 'data-function="show-tooltip-text"'
   },
-  secondButton: {
-    attributes: 'data-function="close-remove-task-element-dialog-button"',
-    text: 'No'
+  children: Icon({ variant: ICON_VARIANTS_ENUM.FLAG, props: `width=10px stroke-width="0.8" fill=${iconColor} color=${iconColor} data-function="show-task-element-priority" ` }),
+  dropdownListItems: TaskElementDropdownListItems(),
+  buttonAttributes: `data-function="show-task-element-priority-button" data-id=${id}`,
+  dropdownContentAttributes: 'data-function="show-dropdown-content"'
+})
+
+export const getRemoveButtonProps = ({ taskName, id }) => ({
+  variant: BUTTON_VARIANTS_ENUM.REMOVE,
+  buttonAttributes: ` data-function="show-remove-task-element-dialog" data-id=${id}`,
+  confirmDialogProps: {
+    variant: DIALOG_VARIANTS_ENUM.CONFIRMATION,
+    dialogAttributes: 'data-function="remove-task-element-dialog"',
+    text: `Are you sure you want to delete the "${taskName}" task ?`,
+    firstButton: {
+      attributes: 'data-function="remove-task-element-button"',
+      text: 'Yes'
+    },
+    secondButton: {
+      attributes: 'data-function="close-remove-task-element-dialog-button"',
+      text: 'No'
+    }
   }
 })
